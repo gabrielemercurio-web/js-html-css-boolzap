29f1e8a10aa1593b132708ccd9c49122479046fd
Gabriele Mercurio <gabrielemercurio.web@gmail.com>
2020-05-08 12:42:35 +0200

    Start Index + CSS

---
+++ main.js
@@ -0,0 +1,14 @@
+// Scrivere un numero random tra 0 e 10 dentro ognuno dei quadrati
+
+// genero 10 numeri random
+$('.quadrato').each(function() {
+
+    var numero_random = generaRandom(0, 10);
+    $(this).text(numero_random);
+
+});
+
+function generaRandom(min, max) {
+  var random = Math.floor(Math.random() * (max - min + 1) ) + min;
+  return random
+}
