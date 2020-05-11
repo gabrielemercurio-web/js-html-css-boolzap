2edc636306c51528b0c79fd94c7b1de4c4c4eb9a
Gabriele Mercurio <gabrielemercurio.web@gmail.com>
2020-05-11 22:18:19 +0200

    MileStone-1 Completata

--- main.js
+++ main.js
@@ -1,14 +1,25 @@
-// Scrivere un numero random tra 0 e 10 dentro ognuno dei quadrati
+// intercettare il click dell'utente sulla classe .invia-msg
 
-// genero 10 numeri random
-$('.quadrato').each(function() {
+$('.invia-msg').click(function () {
 
-    var numero_random = generaRandom(0, 10);
-    $(this).text(numero_random);
+    // intercettare il messaggio che l'utente scriverà nell'input
+    // salvare il messaggio scritto dell'utente in una var
+    var messaggio_utente = $('.scrivi-messaggio input').val();
+    console.log(messaggio_utente);
+
+    var bubble = $('.template .mio-bubble-msg').clone();
+
+    // inserire il messaggio_utente nel template bubble in html
+    bubble.find('p.text-message').text(messaggio_utente);
+
+    // inviare il messaggio
+
+    if ($('#input-msg').val() == '') {
+        alert('Errore: input vuoto!');
+    } else {
+        $('.right-center').append(bubble);
+    };
+
+    $('#input-msg').val('');
 
 });
-
-function generaRandom(min, max) {
-  var random = Math.floor(Math.random() * (max - min + 1) ) + min;
-  return random
-}
