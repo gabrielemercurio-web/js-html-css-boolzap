a207d2f9a832b19a7d86eba4b02059587421934a
Gabriele Mercurio <gabrielemercurio.web@gmail.com>
2020-05-12 16:22:09 +0200

    SetTimeout per Risposta Automatica

--- main.js
+++ main.js
@@ -1,13 +1,22 @@
 // intercettare il click dell'utente sulla classe .invia-msg
 
-$('.invia-msg').click(function () {
+$('.invia-msg').click(invia_messaggio);
 
+// intercettare il tasto ENTER dell'utente sull'id #input-msg
+$('#input-msg').keypress(function(event) {
+    if (event.which == 13) {
+        invia_messaggio();
+    }
+});
+
+function invia_messaggio (){
     // intercettare il messaggio che l'utente scriverà nell'input
     // salvare il messaggio scritto dell'utente in una var
     var messaggio_utente = $('.scrivi-messaggio input').val();
-    console.log(messaggio_utente);
 
-    var bubble = $('.template .mio-bubble-msg').clone();
+    var bubble = $('.template .bubble-msg').clone();
+
+    bubble.addClass('mio-bubble-msg');
 
     // inserire il messaggio_utente nel template bubble in html
     bubble.find('p.text-message').text(messaggio_utente);
@@ -22,4 +31,39 @@ $('.invia-msg').click(function () {
 
     $('#input-msg').val('');
 
-});
+
+    // Messaggio di Risposta
+    setTimeout(function() {
+
+        var bubble_risposta = $('.templete .bubble-msg').clone();
+
+        bubble_risposta.addClass('contatto-bubble-msg');
+
+        bubble_risposta.find('p.text-message').text('Ok!');
+
+        $('.right-center').append(bubble_risposta);
+
+        console.log(bubble_risposta);
+
+    }, 1000);
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// END
