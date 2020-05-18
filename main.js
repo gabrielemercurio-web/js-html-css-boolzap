a30a53ee04083fc482e87deb7c4ea363f2ca4166
Gabriele Mercurio <gabrielemercurio.web@gmail.com>
2020-05-18 15:05:38 +0200

    MileStone 3.1 (da capire cosa non funziona)

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ $('#input-msg').keypress(function(event) {
 function invia_messaggio (){
     // intercettare il messaggio che l'utente scriverà nell'input
     // salvare il messaggio scritto dell'utente in una var
-    var messaggio_utente = $('.scrivi-messaggio input').val();
+    var messaggio_utente = $('input#input-msg').val();
 
     var bubble = $('.template .bubble-msg').clone().prepend('<h1>Gabriele</h1>');
 
@@ -22,31 +22,38 @@ function invia_messaggio (){
     bubble.find('p.text-message').text(messaggio_utente);
 
     // inviare il messaggio
-
-    if ($('#input-msg').val() == '') {
+    // se il campo dell'input è vuoto
+    if ($('input#input-msg').val() == '') {
         alert('Errore: input vuoto!');
     } else {
+        // Altrimenti, inserisco il bubble con il mio messaggio dentro allo spazio centrale
+        $('.right-center.active').append(bubble);
+        // Svuoto il campo input
+        $('#input-msg').val('');
 
-        // Inserisco il bubble con il mio messaggio dentro allo spazio centrale
-        $('.right-center').append(bubble);
-
-// Messaggio di Risposta automatica dopo 1 secondo
+        // Messaggio di Risposta automatica del PC dopo 1 secondo
         setTimeout(function() {
 
-            var bubble_risposta = $('.template .bubble-msg').clone().prepend('<h1>Pdor</h1>');
+            var bubble_risposta = $('.template .bubble-msg').clone();
+
+            var chat_active = $('.active').data('single-chat');
+
+            $(bubble_risposta).prepend('<h1>' + chat_active + '</h1>');
 
             bubble_risposta.addClass('contatto-bubble-msg');
 
             bubble_risposta.find('p.text-message').text('Ok!');
 
-            $('.right-center').append(bubble_risposta);
+            $('.right-center.active').append(bubble_risposta);
 
         }, 1000);
     };
 
-    $('#input-msg').val('');
 
 };
+
+
+
 
 
 // ---------------------------------- Input Cerca
@@ -89,13 +96,24 @@ $('.cerca-wrapper input.search-contact').keyup(function() {
     })
 });
 
+
+
+
+
+// Al click di una single-chat visualizzo la relativa conversazione a destra
 $('.single-chat').click(function() {
 
-    $('#wp-right').removeClass('active');
+    $('.right-center').removeClass('active');
 
     var single_chat = $(this).find('.name p').text();
 
-    $('#wp-right[data-single-chat="'+ single_chat +'"]').addClass('active');
+    $('.right-center[data-single-chat="'+ single_chat +'"]').addClass('active');
+
+    $('#input-msg').keypress(function(event) {
+        if (event.which == 13) {
+            invia_messaggio();
+        }
+    });
 });
 
 
